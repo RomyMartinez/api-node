80580ca6037b507bfbd27be99295a34c1cfd6fd2
RomyMartinez <romy.nava@gsuite.iff.edu.br>
2025-09-02 16:34:57 -0300

    feat: update database configuration
    - Update database connection setup
    - Improve database initialization logic

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,11 @@
 import setupKnew, { Knex } from "knex";
+import { env } from "./env";
 
 export const config: Knex.Config = {
   useNullAsDefault: true,
   client: "sqlite",
   connection: {
-    filename: "./db/app.db",
+    filename: env.DATABASE_URL,
   },
   migrations: {
     extension: "ts",
