c09047b7447d82d8ac4b65d2f957a0542159e223
RomyMartinez <>
2025-09-02 22:49:25 -0300

    fix: simplify GET transactions response return

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -24,6 +24,6 @@ export async function transactionsRoutes(app: FastifyInstance) {
   });
   app.get("/", async (request, reply) => {
     const transactions = await knex("transactions").select("*");
-    return reply.status(200).send(transactions);
+    return transactions;
   });
 }
